1ac874e63304ab987b7585bea98797ad8d0a5f51
Muhammad Fikri Az Zuhdi <92010060+fikriazzhdi@users.noreply.github.com>
2022-03-28 20:18:26 +0700

    Add files via upload

---
+++ Praktikum_5/class_fruit.php
@@ -0,0 +1,25 @@
+<?php
+class fruit {
+    public $name;
+    public $color;
+    public $weight;
+
+    function set_name($n) {
+        $this->name = $n;
+    }
+    protected function set_color($n) {
+        $this->color = $n;
+    }
+    private function set_weight($n) {
+        $this->weight = $n;
+    }
+
+}
+
+$mango = new fruit();
+$mango->set_name('Mango');
+$mango->set_color('Yellow');
+$mango->set_weight('300');
+
+    echo $mango->set_name;
+?>
